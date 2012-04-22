812183d5cc02b2a5e2e528fb9684d953a39aee2f
peepeep <quinntin@gmail.com>
2012-04-22 20:07:25 +0300

    Added entity class

---
+++ Monopoly server/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopoly_server
+{
+    abstract class Entity
+    {
+        private int id;
+        private int money;
+        private int[] property;
+
+        public abstract void changeMoney(int value);
+    }
+}
