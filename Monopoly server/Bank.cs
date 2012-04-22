0446803cd2ebc466bd8de011a453a5f09433312a
Andrius Sabrauskas <shhabas@gmail.com>
2012-04-22 21:00:54 +0300

    Merge branch 'master' of git://github.com/peepeep/Monopoly into peepeep-master
    Conflicts:
    	Monopoly server/Monopoly server.csproj
    	Monopoly.suo

---
+++ Monopoly server/Bank.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopoly_server
+{
+    public class Bank: Entity
+    {
+        private int id;
+        private int money;
+        private int[] property;
+
+        public override void changeMoney(int value)
+        {
+            this.money += value;
+        }
+    }
+}
---
+++ Monopoly server/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopoly_server
+{
+    public class Player: Entity
+    {
+        private int id;
+        private int money;
+        private int[] property;
+        private int in_jail;
+        Random dice_value = new Random();
+
+        public int rollDice ()
+        {
+            return dice_value.Next(11) + 2;
+        }
+
+        public override void changeMoney(int value)
+        {
+            this.money += value;
+        }
+    }
+}
