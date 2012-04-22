2c79f2d17452fa2b053746978cf11eb360435b2d
Andrius Sabrauskas <shhabas@gmail.com>
2012-04-22 20:26:00 +0300

    Field is up!

---
+++ Monopoly server/Field.cs
@@ -0,0 +1,51 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="field.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Monopoly_server
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public abstract class Field
+    {
+        private int id;       
+        private Bank bank;
+        private Boolean buyable;
+        private Boolean traded_in;
+        private int[] values;
+        private string name;
+
+        private Entity m_owner;
+
+        public Entity owner
+        {
+            get
+            {
+                return m_owner;
+            }
+            set
+            {
+                m_owner = value;
+            }
+        }
+
+        public Field(Bank bank, string name, int[] values)
+        {
+            this.bank = bank;
+            this.name = name;
+            this.values = values;
+        }
+
+        public abstract int work(Player player);
+        
+
+    }
+}
