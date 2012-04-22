dc35ef7d489583aa01dc43181b2e715b4dd36149
peepeep <quinntin@gmail.com>
2012-04-22 20:14:43 +0300

    Added Bank, Player classes

---
+++ Monopoly server/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopoly_server
+{
+    class Player
+    {
+        private int id;
+        private int money;
+        private int[] property;
+        private int in_jail;
+
+        public int rollDice ()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void changeMoney(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
