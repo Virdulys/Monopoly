37717bc5ba7ef12f84cc87f96e90ff41a407f771
Andrius Sabrauskas <shhabas@gmail.com>
2012-04-22 18:04:29 +0300

    A little bit more work done in config parsing

--- Monopoly server/Program.cs
+++ Monopoly server/Program.cs
@@ -10,8 +10,8 @@ namespace Monopoly_server
     {
         static void Main(string[] args)
         {
-            ConfigFile configFile = new ConfigFile("map.ini");
-            Console.WriteLine(configFile.SettingGroups["street19"].Settings["name"].GetValueAsString());
+            ConfigFile configFile = new ConfigFile("../../config/Game.ini");
+            //Console.WriteLine(configFile.SettingGroups["street19"].Settings["name"].GetValueAsString());
             foreach (KeyValuePair<string, SettingsGroup> group in configFile.SettingGroups)
             {
                 Console.WriteLine("****************************");
@@ -19,7 +19,21 @@ namespace Monopoly_server
                 Console.WriteLine();
 
                 foreach (KeyValuePair<string, Setting> value in group.Value.Settings)
-                    Console.WriteLine("{0} = {1} (Is Array? {2})", value.Key, value.Value.RawValue, value.Value.IsArray);
+                {
+                    Console.WriteLine("{0} = {1} (Is Array? {2})", value.Key, value.Value.GetValueAsString(), value.Value.IsArray);
+                    ConfigFile configFile2 = new ConfigFile("../../config/" + value.Value.GetValueAsString());
+                    foreach (KeyValuePair<string, SettingsGroup> group2 in configFile2.SettingGroups)
+                    {
+                        Console.WriteLine("****************************");
+                        Console.WriteLine(group2.Key + ":");
+                        Console.WriteLine();
+
+                        foreach (KeyValuePair<string, Setting> value2 in group2.Value.Settings)
+                            Console.WriteLine("{0} = {1} (Is Array? {2})", value2.Key, value2.Value.RawValue, value2.Value.IsArray);
+
+                        Console.WriteLine();
+                    }
+                }
 
                 Console.WriteLine();
             }
